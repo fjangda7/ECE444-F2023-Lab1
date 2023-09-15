d36d4bd3f03b78578845f17a8829b91b8a36cf95
Fatima Jangda <59586270+fjangda7@users.noreply.github.com>
2023-09-15 19:58:30 +0000

    completed activity3

---
+++ hello3.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+bootstrap = Bootstrap(app)
+
+@app.route('/') 
+def index():
+    return render_template('index.html')
+
+@app.route('/user/<name>') 
+def user(name):
+    return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
